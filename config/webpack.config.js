e6d426ba9420984bacecdcb24d6c0f6db497a5ff
chrlschwb <chrlschwb77@outlook.com>
2022-04-12 22:43:45 -0800

    browser error fixed

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -295,7 +295,15 @@ module.exports = function (webpackEnv) {
     resolve: {
       fallback:{
         "crypto": require.resolve("crypto-browserify"),
-        "stream": require.resolve("stream-browserify")
+        fs: false,
+        'stream': require.resolve('stream-browserify'),
+        'buffer': require.resolve('buffer/'),
+        'util': require.resolve('util/'),
+        'assert': require.resolve('assert/'),
+        'http': require.resolve('stream-http/'),
+        'url': require.resolve('url/'),
+        'https': require.resolve('https-browserify/'),
+        'os': require.resolve('os-browserify/'),
       },
       // This allows you to set a fallback for where webpack should look for modules.
       // We placed these paths second because we want `node_modules` to "win"
--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import {xx} from './walletInit';
+import './walletProvider';
 
 function App() {
-  xx();
   return (
     <div className="App">
       <header className="App-header">
