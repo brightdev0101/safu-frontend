51690f42092f5b676bae1dd5cab8bff6d3aff9c1
chrlschwb <chrlschwb77@outlook.com>
2022-04-19 07:36:45 -0800

    wallet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
 import {Provider} from "react-redux";
 import store from "./store.js";
 
@@ -19,23 +20,27 @@ class App extends Component {
 
   render()
   {
-    return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-              <Navbar />
-              <Route exact path="/" component={Home}/>
-              <Route exact path="/PadList" component={PadList}/>
-              <Route exact path="/PadInfo" component={PadInfo}/>
-              <Route exact path="/CreateToken" component={CreateToken}/>
-              <Route exact path="/LaunchPad1" component={LaunchPad1}/>
-              <Route exact path="/LaunchPad2" component={LaunchPad2}/>
-              <Route exact path="/LaunchPad3" component={LaunchPad3}/>
-              <Route exact path="/LaunchPad4" component={LaunchPad4}/>
-              <Footer />
-          </div>
-        </Router>
-      </Provider>
+    return (   
+      <MoralisProvider serverUrl="https://rz5hutq1rlrl.usemoralis.com:2053/server" appId="mGHnSx9cVgnPD7LnRnCPZ7UCMhJoYilNGNIKDtO0">
+        <Provider store={store}>
+          <Router>
+            <div className="App">
+            
+                <Navbar />
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/PadList" component={PadList}/>
+                <Route exact path="/PadInfo" component={PadInfo}/>
+                <Route exact path="/CreateToken" component={CreateToken}/>
+                <Route exact path="/LaunchPad1" component={LaunchPad1}/>
+                <Route exact path="/LaunchPad2" component={LaunchPad2}/>
+                <Route exact path="/LaunchPad3" component={LaunchPad3}/>
+                <Route exact path="/LaunchPad4" component={LaunchPad4}/>
+                <Footer />
+            </div>
+          </Router>
+        </Provider>
+      </MoralisProvider>
+      
     );
   }
 }
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -2,103 +2,54 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import "../layout/styles.css";
 import Logo from './img/logo.png';
-
-import Web3Modal from "web3modal";
-import { ethers } from 'ethers';
-import { useEffect, useState } from 'react';
-import {providerOptions} from '../../utils/walletProvider';
+import MetamaskImg from "./img/metamask.jpg";
+import WalletconnectImg from "./img/walletconnect.png";
+import { useMoralis } from "react-moralis";
 
 const Navbar = () => {
 
-    const [provider, setProvider] = useState();
-    const [library, setLibrary] = useState();
-    const [account, setAccount] = useState();
-    const [network, setNetwork] = useState();
-    const [error, setError] = useState("");
-    const [chainId, setChainId] = useState();
-  
-    const web3Modal = new Web3Modal({
-      cacheProvider: true,
-      providerOptions 
-    });
-  
-    const connectWallet = async () => {
-        try {
-          const provider = await web3Modal.connect();
-          const library = new ethers.providers.Web3Provider(provider);
-          const accounts = await library.listAccounts();
-          const network = await library.getNetwork();
-          setProvider(provider);
-          setLibrary(library);
-          if (accounts) setAccount(accounts[0]);
-          setNetwork(network);
-          setChainId(network.chainId);
-        } catch (error) {
-          setError(error);
-        }
-    };
-  
-    const refreshState = () => {
-        setAccount("");
-        setChainId();
-        setNetwork();
-        setProvider();
-        setLibrary();
-    };
-  
-    const disconnect = async () => {
-        await web3Modal.clearCachedProvider();
-        refreshState();
-    };
-  
-    useEffect(() => {
-      if (web3Modal.cachedProvider) {
-        connectWallet();
-      }
-    },[]);
-  
-    useEffect(() => {
-      if (provider?.on) {
-        const handleAccountsChanged = (accounts) => {
-          console.log("accountsChanged", accounts);
-          if (accounts) setAccount(accounts[0]);
-        };
-  
-        const handleChainChanged = (_hexChainId) => {
-          setChainId(_hexChainId);
-        };
-  
-        const handleDisconnect = () => {
-          console.log("disconnect", error);
-          disconnect();
-        };
-  
-        provider.on("accountsChanged", handleAccountsChanged);
-        provider.on("chainChanged", handleChainChanged);
-        provider.on("disconnect", handleDisconnect);
-  
-        return () => {
-          if (provider.removeListener) {
-            provider.removeListener("accountsChanged", handleAccountsChanged);
-            provider.removeListener("chainChanged", handleChainChanged);
-            provider.removeListener("disconnect", handleDisconnect);
-          }
-        };
-      }
-    }, [provider]);
+  const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
 
-    
+  const login = async () => {
+    if (!isAuthenticated) {
+      await authenticate({signingMessage: "Connect to SaFuTrendzPad" })
+        .then(function (user) {
+          console.log("logged in user:", user);        
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
+  const login2 = async () => {
+    if (!isAuthenticated) {
+      await authenticate({signingMessage: "Connect to SaFuTrendzPad",provider:"walletconnect" })
+        .then(function (user) {
+          console.log("logged in user:", user);        
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
+  const logOut = async () => {
+    await logout();
+    console.log("logged out");
+  }
+
+
        
     return ( 
     <>
         <nav className = "navbar navbar-expand-md bg-dark navbar-dark" style={{textAlign: 'left', color: '#fff', backgroundColor: '#fff'}}>
         
             <a href = "/" ><img src = { Logo } className = "photo"  alt = "logo" style={{marginLeft: '-35px'}} /></a>
-            <button onClick={!account ? connectWallet : disconnect } className="btn btn-success" style={{backgroundImage: 'linear-gradient(135deg,#ebd15f,#fa0)', marginLeft:'20px'}}>
-              <span>{!account ? "Connect" : "Disconnect"}</span></button>
-
-            {/* <p>Wallet Address: {account}</p> */}
             
+            <img onClick={login} width="40" src={MetamaskImg} alt="m" />
+            <img onClick={login2} width="40" src={WalletconnectImg} alt="w" />
+
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                 <span className="navbar-toggler-icon"></span>
             </button>
